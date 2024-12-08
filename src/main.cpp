626ddc81be92af1ad1ef707b3235167a513ac783
Patrick Schuur <patrickschuurdev@gmail.com>
2024-12-08 17:07:25 +0100

    Raylib working

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <filesystem>
+
+#include "raylib.h"
+
+#ifndef RESOURCES_PATH
+#define RESOURCES_PATH std::string("resources/")
+#endif
+
+#if defined(PLATFORM_WEB)
+	#include <emscripten/emscripten.h>
+#endif
+
+static void UpdateDrawFrame() {
+	BeginDrawing();
+	ClearBackground(RED);
+	DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+	EndDrawing();
+}
+
+constexpr int TARGET_FPS = 60;
+
+
+int main() {
+	
+	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitAudioDevice();
+
+
+#if defined(PLATFORM_WEB)
+	emscripten_set_main_loop(UpdateDrawFrame,TARGET_FPS,1);
+#else
+
+	SetTargetFPS(TARGET_FPS);
+	
+	while (!WindowShouldClose())
+	{
+		UpdateDrawFrame();
+	}
+#endif
+
+	CloseAudioDevice();
+	CloseWindow();
+
+	return 0;
+}
